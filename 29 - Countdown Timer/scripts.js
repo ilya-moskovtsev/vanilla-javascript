b5de33ad6b60e3ecce4f9c2b6b364c0104139daa
ilya-moskovtsev <ilya.moskovtsev@gmail.com>
2018-07-01 00:45:32 +0300

    add solution for 29 - Countdown Timer

--- 29 - Countdown Timer/scripts.js
+++ 29 - Countdown Timer/scripts.js
@@ -4,28 +4,27 @@ const times = timerControls.querySelectorAll('[data-time]');
 const timeLeft = document.querySelector('.display__time-left');
 const endTime = document.querySelector('.display__end-time');
 
-let secondsLeft = 0;
+let backAt;
+let secondsLeft;
 let intervalID;
 
 function timer() {
-    --secondsLeft;
-    displayTimeLeft(secondsLeft);
+    secondsLeft = (backAt.getTime() - Date.now()) / 1000;
+    displayTimeLeft();
     if (secondsLeft <= 0) {
         clearInterval(intervalID);
     }
 }
 
-function displayTimeLeft(secondsLeft) {
-    const minutesLeft = Math.floor(secondsLeft / 60);
-    const remainderSecondsLeft = secondsLeft % 60;
+function displayTimeLeft() {
+    const minutesLeft = Math.round(secondsLeft / 60);
+    const remainderSecondsLeft = Math.round(secondsLeft % 60);
     const timeLeftStr = `${minutesLeft < 10 ? '0' : ''}${minutesLeft}:${remainderSecondsLeft < 10 ? '0' : ''}${remainderSecondsLeft}`;
     document.title = timeLeftStr;
     timeLeft.textContent = timeLeftStr;
 }
 
 function startTimer(e) {
-    clearInterval(intervalID);
-
     //set secondsLeft
     if (e.type === 'submit') {
         e.preventDefault();
@@ -34,12 +33,15 @@ function startTimer(e) {
     } else {
         secondsLeft = this.dataset.time;
     }
+    if (secondsLeft <= 0) return;
 
-    displayTimeLeft(secondsLeft);
+    backAt = new Date(Date.now() + secondsLeft * 1000);
 
-    const backAt = new Date(Date.now() + secondsLeft * 1000);
-    endTime.textContent = `Be back at ${backAt.getHours()}:${backAt.getMinutes()}`;
+    displayTimeLeft();
 
+    endTime.textContent = `Be back at ${backAt.getHours() < 10 ? '0' : ''}${backAt.getHours()}:${backAt.getMinutes() < 10 ? '0' : ''}${backAt.getMinutes()}`;
+
+    clearInterval(intervalID);
     intervalID = setInterval(timer, 1000);
 }
 
